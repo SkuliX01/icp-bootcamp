0e631ace2ef3b39de76d3e0c0d01fb352386e211
SkuliX01 <bartosz.krawczyk34@gmail.com>
2024-06-24 18:54:50 +0200

    Small changes

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -3,5 +3,6 @@
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <h1>Siemka 👋 Jestem SkuliX</h1>
+    <h2>I jestem Programaista Backend w jezykach TypeScript oraz Go</h2>
   </main>
 </template>
